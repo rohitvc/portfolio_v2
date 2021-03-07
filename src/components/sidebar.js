dc23116be0790df7d7329744d1559c873e4a4e56
Rohit Chavan <rohitchavanrohit96@gmail.com>
2021-03-07 12:55:57 +0530

    [WIP] part 1 - header

---
+++ src/components/sidebar.js
@@ -0,0 +1,37 @@
+import React from "react"
+import './sidebar.css';
+
+const Sidebar = ({}) => {
+  return (
+    <div className="h-screen">
+      <nav className="h-full">
+        <ul className="min-h-35 max-h-90 flex flex-col justify-between">
+          <li className="flex justify-center mt-4">
+            <div className="rounded-full flex items-center justify-center items-center h-16 w-16 bg-red-600">
+              <h5 className="text-white text-xl hover:text-gray-800 cursor-pointer">
+                RC
+              </h5>
+            </div>
+          </li>
+          <li className="sidebar-menu-item inline-block transform -rotate-90 origin-center text-center hover:text-red-500 cursor-pointer">
+            Home
+          </li>
+          <li className="sidebar-menu-item inline-block transform -rotate-90 origin-center text-center hover:text-red-500 cursor-pointer">
+            About
+          </li>
+          <li className="sidebar-menu-item inline-block transform -rotate-90 origin-center text-center hover:text-red-500 cursor-pointer">
+            Experience
+          </li>
+          <li className="sidebar-menu-item inline-block transform -rotate-90 origin-center text-center hover:text-red-500 cursor-pointer">
+            Works
+          </li>
+          <li className="sidebar-menu-item inline-block transform -rotate-90 origin-center text-center hover:text-red-500 cursor-pointer">
+            Contact
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+    },
+    maxHeight: {
+      '90': '90%',
+      '60vh': '60vh',
+    },
+    minHeight: {
+      '35': '35rem',
+    }
+  },
+  variants: {
+    extend: {
+      fill: ['hover', 'focus'],
+    },
+  },
+  plugins: [],
+}
